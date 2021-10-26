f2d6ddbad48f8bbf882559a1385d16ca6ce43937
Vítězslav Kříž <sprtokiller.6c@gmail.com>
2021-10-26 22:00:55 +0200

    Added skeleton of Symtable (for variables)

---
+++ include/symtable.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Xstring.h"
+#include "token.h"
+
+#define TABLE_SIZE 100 //Change this later
+
+//A symbol, represented as a hash table item
+typedef struct hashtable_v_item hashtable_v_item;
+
+//dont use this!
+//just for editors autocomplete
+struct hashtable_v_item
+{
+	String identifier;
+	struct hashtable_v_item_struct* next_synonym;
+	token_type_t token_type;
+	uint32_t frame_count;
+};
+
+typedef hashtable_v_item* Sym_v_table[TABLE_SIZE];
+
+void Sym_v_table_dtor(Sym_v_table* self);
+
+void Sym_v_table_insert(Sym_v_table* self, char* id, token_type_t token_type, uint32_t frame_count);
+
+hashtable_v_item* Sym_v_table_find(Sym_v_table* self, char* id);
+
+uint32_t hash(char* id);
---
+++ src/symtable.c
@@ -0,0 +1,26 @@
+#include "symtable.h"
+
+void Sym_v_table_dtor(Sym_v_table* self)
+{
+	return;
+}
+
+void Sym_v_table_insert(Sym_v_table* self, char* id, token_type_t token_type, uint32_t frame_count)
+{
+	return;
+}
+
+hashtable_v_item* Sym_v_table_find(Sym_v_table* self, char* id)
+{
+	return NULL;
+}
+
+//fake hash function
+uint32_t hash(char* id)
+{
+	if (id)
+	{
+		return (uint32_t)id[0] % TABLE_SIZE;
+	}
+	return 0;
+}
