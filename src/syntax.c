7f2eb4531629dd1516a766db38aa488e79682e1a
Vítězslav Kříž <sprtokiller.6c@gmail.com>
2021-12-07 16:35:59 +0100

    typou

--- src/syntax.c
+++ src/syntax.c
@@ -856,7 +856,7 @@ void prg_generate(Program* self, struct CodeGen* codegen)
 	append_str(&codegen->global, "DEFVAR GF@__XTMP_STR\n"
 		"DEFVAR GF@__XTMP_STRLEN\n\n");
 	blk_generate(&self->global_block, codegen);
-	append_str(&codegen->global, "EXIT 0\n\n");
+	append_str(&codegen->global, "EXIT int@0\n\n");
 	CG_AddBuiltins(codegen);
 }
 
